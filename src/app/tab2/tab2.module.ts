9d6854f22696a4a872568615019d2cc2c7418827
matheushonoratodev <matheushonorato230@gmail.com>
2023-08-09 12:50:55 -0300

    app dating

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -13,8 +14,10 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab2PageModule {}
